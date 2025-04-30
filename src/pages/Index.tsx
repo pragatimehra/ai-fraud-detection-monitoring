63f4e759cef6303fd76c9f9e4294faff77d0a141
Pragati Mehra <92671158+pragatimehra@users.noreply.github.com>
2025-04-30 11:13:34 +0530

    Delete src directory

--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-
-import { Navigate } from "react-router-dom";
-
-const Index = () => {
-  // Just redirect directly to dashboard
-  return <Navigate to="/dashboard" replace />;
-};
-
-export default Index;
